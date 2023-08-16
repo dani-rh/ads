9ee824954f0f4a9affe40d4cc417501d03411969
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-08-16 17:40:42 -0300

    Initial commit

---
+++ anotacoes.py
@@ -0,0 +1,20 @@
+print("Alo alo, testando.")
+print("Olá de novo!")
+print(5 + 1)
+print("Olá" + " " + "de novo!")
+print(len("Python"))
+print(type(5))
+print(type("Python"))
+print(max([1, 7, 2, 3]))
+print(min([1, 7, 2, 3]))
+print(sum([1, 7, 2, 3]))
+print(round(3.14159265359, 2))
+print(bool(1))
+print(bool(0))
+print(str(123))
+print('Olá de novo!'.split())#divide a string em uma lista de palavras a cada espaço
+print("wellington".capitalize())#primeira letra em maiuscula
+print(abs(-5))#retorn o número absoluto
+print(" São José dos Pinhais ".strip())#remove os espaços em branco no início e no fim da string, mas vai ignorar os espaços entre as palavras
+print("Olá de novo!".replace("novo", "volta"))
+print(list(range(10)))
