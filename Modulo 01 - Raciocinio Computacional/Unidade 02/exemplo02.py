251e9197c1fd4768954040e2dce336b7123d7086
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-08-22 15:41:23 -0300

    Adicionar exemplo 02 unidade 02

---
+++ Modulo 01 - Raciocinio Computacional/Unidade 02/exemplo02.py
@@ -0,0 +1,16 @@
+# Exemplo 02 - Selecao simples
+# Solicita as notas
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+
+# Calcule a media
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+# Verifica se o estudante foi aprovado
+if media >= 7:
+    print("O estudante foi aprovado.")
+
+# Exibe a média
+print(f"A média do estudante é {media:.2f}")
