b32ee44a17e01535c37f613549e8f0cae61a0eaa
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-08-22 15:41:58 -0300

    Adicionar exemplo 02 unidade 03

---
+++ Modulo 01 - Raciocinio Computacional/Unidade 03/exemplo02.py
@@ -0,0 +1,14 @@
+# soma = 0
+# for i in range(3):
+#     num = int(input("Digite o " + str(i+1) + " número:"))
+#     soma += num
+# print("A soma dos números é", soma)
+
+for i in range (5):
+    print (i)
+    
+for i in range (2, 7):
+    print(i)
+    
+for i in range (4, 12, 2):
+    print(i)
