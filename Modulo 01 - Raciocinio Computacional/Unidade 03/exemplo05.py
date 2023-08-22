313c9f024af4fa722c407a9d538e8e856a711ec4
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-08-22 15:42:19 -0300

    Adicionar exemplo 05 unidade 03

---
+++ Modulo 01 - Raciocinio Computacional/Unidade 03/exemplo05.py
@@ -0,0 +1,9 @@
+# mult = 1
+# for i in range (10):
+#     num = int(input("Digite o " + str(i + 1)+ " número:"))
+#     if num == 0:
+#         continue
+#     mult *= num
+# print("A multiplicação dos números é", mult)
+
+for i in range(4):
