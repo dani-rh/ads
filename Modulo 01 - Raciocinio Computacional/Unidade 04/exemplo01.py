5da55133305a80ed2a8335604d559be725765ab4
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-08-24 16:51:12 -0300

    Adicionar exemplo 01

---
+++ Modulo 01 - Raciocinio Computacional/Unidade 04/exemplo01.py
@@ -0,0 +1,12 @@
+# nums = [0, 0, 0, 0, 0]
+# for i in range (5):
+#     num = int(input("Digite um número: "))
+#     nums[i] = num
+# print(nums)
+
+# Para se tornar dinamico, é possível usar o método append
+nums = []
+for i in range (10):
+    num = int(input("Digite um número: "))
+    nums.append(num)
+print(nums)
