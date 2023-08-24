a3b506a2728b341cbde16bfe979c685a7b8d9eb9
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-08-24 18:35:22 -0300

    Adicionar exemplo 04

---
+++ Modulo 01 - Raciocinio Computacional/Unidade 04/exemplo04.py
@@ -0,0 +1,9 @@
+nums = [17, 33, 23, 11, 8, 15, 9]
+maior = nums[0]
+menor = nums[0]
+for num in nums:
+    if num > maior:
+        maior = num
+    if num < menor:
+        menor = num
+print("O maior número da lista é", maior,"e o menor é",menor)
