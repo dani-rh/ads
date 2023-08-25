09317aacb2e8795af3c00f3944c9822d25546966
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-08-25 14:40:06 -0300

    Adicionar exercicio 06

---
+++ Modulo 01 - Raciocinio Computacional/Unidade 04/exercicio06.py
@@ -0,0 +1,6 @@
+'''Exercício de fixação 6: Dado o vetor nums = [3, 7, 2, 9, 5, 6], crie um programa que, em uma linha, calcule a média dos seus elementos.'''
+
+nums = [3, 7, 2, 9, 5, 6]
+
+media = sum(nums)/len(nums)
+print("A média dos elementos do vetor é", media)
