5b16b0e8d80c3f27c45c0a7a43f139b71256340f
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-08-25 14:40:00 -0300

    Adicionar exercicio 05

---
+++ Modulo 01 - Raciocinio Computacional/Unidade 04/exercicio05.py
@@ -0,0 +1,13 @@
+'''A matriz-identidade é uma matriz com a mesma quantidade de linhas e colunas, apresentando todos os elementos da diagonal principal (de cima para baixo, da esquerda para a direita) iguais a 1 e os demais elementos iguais a 0. Crie um programa que solicite o tamanho da matriz desejada, gerando a matriz-identidade.'''
+
+identidade = []
+tamanho = int(input("Qual é o tamanho da matriz-identidade desejada? "))
+for i in range(tamanho):
+    identidade.append([])
+    for j in range(tamanho):
+        if i == j:
+            identidade[i].append(1)
+        else:
+            identidade[i].append(0)
+for linha in identidade:
+    print(linha)
