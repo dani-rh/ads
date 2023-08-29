f77d9d0dfed6322d4589f3aa2fc068fc5e8c3d99
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-08-29 16:45:32 -0300

    Adicionar exemplo 06

---
+++ Modulo 01 - Raciocinio Computacional/Unidade 05/exemplo06.py
@@ -0,0 +1,6 @@
+# Concatenar tuplas
+endereco_puc = ("Rua Imaculada Conceição", 1555, "Prado Velho", "Curitiba", "PR")
+print(id(endereco_puc))
+endereco_puc += ("Brazil",)
+print(endereco_puc)
+print(id(endereco_puc))
