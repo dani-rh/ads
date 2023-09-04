ddda3c5405d9983754419eee6ab8b3b5714b6c11
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-09-04 19:18:38 -0300

    Adicionar exemplo 04

---
+++ Modulo 01 - Raciocinio Computacional/Unidade 06/exemplo04.py
@@ -0,0 +1,13 @@
+def maior_menor(*numeros):
+    maior = -1000000
+    menor = 100000
+    for numero in numeros:
+        if numero > maior:
+            maior = numero
+        if numero < menor:
+            menor = numero
+    return maior, menor
+
+resultado = maior_menor(7, 15, 3, 22, 1, 8)
+print(f"O maior número é {resultado[0]} e o menor número é {resultado[1]}")
+
