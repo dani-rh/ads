c99ab5fcab46d94c4d0eba622dc7317211452ef4
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-09-19 19:25:55 -0300

    Adicionar exercicio 01

---
+++ Modulo 01 - Raciocinio Computacional/Unidade 08/exercicio01.py
@@ -0,0 +1,37 @@
+'''Exercício de fixação 1: Crie uma classe Televisao (sem acento), em que o usuário é capaz de trocar de canais e aumentar/diminuir o volume. O limite de canais válidos vai de 1 a 15 e o limite de volume válido vai de 0 a 10. No caso de um canal inválido, deve ser definido como padrão o canal 1.'''
+
+class Televisao:
+    def __init__(self):
+        self.__canal = 1
+        self.__volume = 0
+        
+    def aumentar_volume(self):
+        volume = self.__volume + 1
+        self.__volume = volume if volume <= 10 else 10
+        self.__imprimir()
+        
+    def diminuir_volume(self):
+        volume = self.__volume - 1
+        self.__volume = volume if volume >= 0 else 0
+        self.__imprimir()
+        
+    def trocar_canal(self, canal):
+        if 1 <= canal <= 15:
+            self.__canal = canal
+        else:
+            self.__canal = 1
+        self.__imprimir()
+        
+    def __imprimir(self):
+        print("Televisão")
+        print("Volume:", self.__volume, "- Canal:", self.__canal)
+        
+
+if __name__ == '__main__':
+    televisao = Televisao()
+    televisao.trocar_canal(5)
+    televisao.aumentar_volume()
+    televisao.aumentar_volume()
+    televisao.trocar_canal(20)
+    televisao.diminuir_volume()
+    
