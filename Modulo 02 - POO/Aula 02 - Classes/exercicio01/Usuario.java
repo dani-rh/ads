54e51c1c846b5b32393a11a44d6da9b64d7c351f
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-10-17 19:48:14 -0300

    Adicionar exercicios

---
+++ Modulo 02 - POO/Aula 02 - Classes/exercicio01/Usuario.java
@@ -0,0 +1,13 @@
+package exercicio01;
+/*1. Desenvolva um projeto em Java contendo somente um arquivo: Usuario.java. Esse arquivo deve conter uma classe chamada Usuario, que representa um usuário da Netflix. Essa classe precisa de três atributos (nome do usuário, e-mail e senha), somente.*/ 
+public class Usuario {
+    // Atributos da classe
+    String nome;
+    String email;
+    String senha;
+
+    //Método principal
+    public static void main(String[] args){
+        
+    }
+}
