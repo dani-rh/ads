6da657dc8c229e7d33baf3ef818d4fe3357a9143
Daniela Haeitmann <danielahaeitmann@gmail.com>
2023-11-06 15:09:46 -0300

    Adicionar exemplo 02 unidade 05

---
+++ Modulo 02 - POO/Aula 05 - Herança/Exemplo02/Moto.java
@@ -0,0 +1,20 @@
+package Exemplo02;
+
+public class Moto extends Veiculo{
+
+    private int cilindradas;
+    
+    public Moto(String marca, String motor, int cilindradas) {
+        super(marca,motor);
+        this.cilindradas = cilindradas;
+    }
+
+    public int getCilindradas() {
+        return cilindradas;
+    }
+    
+    public String imprimir() {
+        return "Marca: " + getMarca() + ", Motor: " + getMotor() + 
+            " de " + getCilindradas() + " cilindradas.";                 
+    }
+}
